f48eccaf7c0b275ad1d1ab37da0bd9e752dc7fcd
CASHWINDRAW <89193783+CASHWINDRAW@users.noreply.github.com>
2021-08-20 19:22:44 -0800

    Update TwitterCardMeta.tsx

--- src/components/meta/TwitterCardMeta.tsx
+++ src/components/meta/TwitterCardMeta.tsx
@@ -9,7 +9,7 @@ type Props = {
 export default function TwitterCardMeta({ url, title, description }: Props) {
   return (
     <Head>
-      <meta property="twitter:cards" content="summary_large_image" />
+      <meta property="twitter:card" content="summary_large_image" />
       <meta property="twitter:site" content={config.twitter_account} />
       <meta property="twitter:url" content={config.base_url + url} />
       <meta
